f90f4eccaa17ebbbd519eff6677628e7d23a4569
RaghadAbd <116813849+RaghadAbd@users.noreply.github.com>
2023-03-15 20:26:13 +0200

    Update ActivityLecture2_ControlFlow.php

--- Activities on the lectures/ActivityLecture2_ControlFlow.php
+++ Activities on the lectures/ActivityLecture2_ControlFlow.php
@@ -1,15 +1,15 @@
 <?php
-/*
+
+//drop down select box from the year 2000 to the year 2023
 echo "<select>";
 for($i=2000;$i<=2023;$i++){
     echo"<option>$i</option>";
 }
 echo "</select><br>";
-*/
 
+echo "<br>----------------------------------<br>";
 
-/*
-//Q1
+//Find Largest Number among three Numbers
 $x=10;
 $y= 5;
 $z=3;
@@ -23,10 +23,11 @@ else if($y>$x && $y>$z){
 else{
       echo $z;
   }
-*/
 
-//Q2 
-/*
+echo "<br>----------------------------------<br>";
+
+//A counter from 0 to 10, use forloop ,that stops when it reaches 5
+
 for($i=0;$i<=10;$i++){
     if($i==5){
         echo "break<br>";
@@ -36,7 +37,10 @@ for($i=0;$i<=10;$i++){
     echo"$i<br>";
 }
 
-*/
+echo "<br>----------------------------------<br>";
+
+//A counter from 0 to 10, use forloop , When it gets to 5 it print "Loop go to next iteration at i=5<br>" and then keeps counting.
+//Using continue and while
 
 $i=0;
     while($i<10){
