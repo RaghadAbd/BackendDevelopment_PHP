e1e2026360e05b810b618a92d0dceab39fa7997e
RaghadAbd <116813849+RaghadAbd@users.noreply.github.com>
2023-03-15 15:56:20 +0200

    Update Lecture3_func.php

--- Lecture explanation/Lecture3_func.php
+++ Lecture explanation/Lecture3_func.php
@@ -1,19 +1,30 @@
 <?php
 
+//Define a function that prints "This is a function".
 function func(){
-
     echo"This is Function <br>";
 }
+//function call
 func();
 
+echo "-----------------------------------------------";
+
+//Function takes two parameter and calculate( + ) using echo 
 function sumFunc(int $x , int $y){
     echo $x + $y ."<br>";
 }
 sumFunc(3,7);
 
+echo "-----------------------------------------------";
+
+//Function takes two parameters and returns their sum
 function sumFunc2 ($x , $y){
     $z = $x + $y;
     return $z;
 }
 echo sumFunc2(2,2);
+
+
+
+
 ?>
